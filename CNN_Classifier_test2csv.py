59c6c4bc5d6bfc1cdafcc791d4f58f6e4aa00533
lchzs <144754906+lchzs@users.noreply.github.com>
2024-07-06 16:20:59 +0800

    Create CNN_Classifier_test2csv.py

---
+++ CNN_Classifier_test2csv.py
@@ -0,0 +1,73 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import pandas as pd
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
+import os
+from PIL import Image
+from models import MyModel, MyModel2,VGG,ResNet18
+
+# 检查cuda是否可用
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+class MyDataset(Dataset):
+    def __init__(self, root, csv_file, transform=None):
+        self.root = root
+        self.transforms = transform
+        self.df = pd.read_csv(csv_file, header=None, skiprows=1)
+        self.classes = sorted(self.df[1].unique())
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, index):
+        vid, label = self.df.iloc[index, :]
+        img_list = os.listdir(os.path.join(self.root, f"{vid}.mp4"))
+        img_list = sorted(img_list)
+        img_path = os.path.join(self.root, f"{vid}.mp4", img_list[int(len(img_list)/2)])
+ 
+        img = Image.open(img_path).convert('RGB')
+        if self.transforms is not None:
+            img = self.transforms(img)
+
+        label = self.classes.index(label)
+        return img, label
+
+transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor()
+])
+
+test_dataset = MyDataset("/home/yujint/rgb/videos", "test.csv", transform)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+# 加载模型
+net = ResNet18().to(device)
+net.load_state_dict(torch.load('/home/yujint/hw2/ResNet18_model_best.pth'))
+
+# 测试模型
+net.eval()
+result = []
+with torch.no_grad():
+    for data in test_loader:
+        inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = net(inputs)
+        _, predicted = torch.max(outputs.data, 1)
+        result.extend(predicted.cpu().numpy())
+        
+fread = open("test.csv", "r")
+video_ids = []
+for line in fread.readlines():
+    video_id = os.path.splitext(line.strip())[0]
+    video_ids.append(video_id)
+
+# df_result = pd.DataFrame(result, columns=['Category'])
+# df_result.to_csv('result.csv', index=False)
+
+with open('result_ResNet18.csv', "w") as f:
+    f.writelines("Id,Category\n")
+    for i, pred_class in enumerate(result):
+        f.writelines("%s,%d\n" % (video_ids[i], pred_class))
